1c246656a373f479e32567ba81c9a9a5ce6e74a8
VictorCher <192.168.127.1@mail.ru>
2019-01-17 17:09:06 +0300

    Реализовал пузырьковую сортировку массива

--- AlgoritmicAndStuctureData/Program.cs
+++ AlgoritmicAndStuctureData/Program.cs
@@ -1,4 +1,4 @@
-﻿// Чернышов Виктор. Урок 2
+﻿// Чернышов Виктор. Урок 3
 
 using System;
 using System.Collections.Generic;
@@ -9,68 +9,58 @@ using System.Threading.Tasks;
 namespace AlgoritmicAndStuctureData
 {
     class Program
-    {   
-        static int answer;
-        static int Task3(int a, int b)
+    {
+        static int [] mass = { 5, 3, 8, 1, 4, 10, 15, 2, 9, 7 };
+        static void Print(string msg, ref int [] mass)
         {
-            /* Задача №3
-         * Исполнитель "Калькулятор" преобразует целое число, записанное на экране.
-         * У исполнителя две команды, каждой присвоен номер:
-         * 1. Прибавь 1
-         * 2. Умножь на 2
-         * Первая команда увеличивает число на экране на 1, вторая увеличивает его в 2 раза.
-         * Сколько существует программ, которые число 3 преобразуют в 20?*/
-            for (int i = 0; i < 2; i++)
+            Console.WriteLine(msg);
+            for (int i = 0; i < mass.Length; i++)
             {
-                int n = a;
-                if (i == 0) n++;
-                else n *= 2;
-                if (n < b) Task3(n, b);
-                else if (n == b) answer++;
+                Console.Write(mass[i] + " ");
             }
-            return answer;
+            Console.WriteLine();
         }
-        static void Task1(int n)
+        //static
+        //static int answer;
+        static int Task1(ref int [] mass)
         {
-            /* Задача №1
-             * Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию.*/
-            int a = n / 2;
-            n = n-(a * 2);
-            if (a >= 2) Task1(a);
-            else Console.Write(a);
-            Console.Write(n);
-        }
-        static int Task2(int a, int b)
-        {
-            /* Задача №2
-             * Реализовать функцию возведения числа а в степень b:
-             * а. Без рекурсии
-             * b. Рекурсивно*/         
-            int result = 0;
-            if ((a == 0) && (b == 0)) return result;
-            result = 1;
-            for (int i = 0; i < b; i++)
+            /* Реализовать пузырьковую сортировку. Вывести количество операций, которые потребовались для сортировки.
+             */
+            int count = 0;
+            Console.WriteLine("Пузырьковая сортировка");
+            Print("Массив до сортировки: ", ref mass);
+            for(int i = 0; i < mass.Length; i++)
             {
-                result *= a;
+               for(int j = 0; j < mass.Length - 1; j++)
+                {
+                    if (mass[j]>mass[j + 1])
+                    {
+                        int x = mass[j];
+                        mass[j] = mass[j + 1];
+                        mass[j + 1] = x;
+                        count++;
+                    }
+                }
             }
-            return result;
+            Print("Массив после сортировки: ", ref mass);
+            return count;
         }
-        static void Main(string[] args)
+        static int Task2(ref int[] mass)
         {
-//          Console.WriteLine("Ответ: " + Task3(3, 20));
- /*           
-            Console.Write("Введите число: ");
-            int n = int.Parse(Console.ReadLine());
-            Console.Write("Bin: ");
-            Task1(n);
-*/
-/*            
-            Console.Write("Введите число: ");
-            int a = int.Parse(Console.ReadLine());
-            Console.Write("В какую степень возвести?: ");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ответ: " + Task2(a, b));
-*/     
+            /* Реализовать шейкерную сортировку. Вывести количество операций, которые потребовались для сортировки.
+             */
+            int count = 0;
+            Console.WriteLine("Шейкерная сортировка");
+
+
+
+            Print("Массив после сортировки: ", ref mass);
+            return count;
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Количество операций: " + Task1(ref mass));
+            Console.WriteLine("Количество операций: " + Task2(ref mass));
             Console.ReadLine();
         }
     }
