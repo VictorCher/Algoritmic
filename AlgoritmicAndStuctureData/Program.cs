9e60cf7494728819521b959baace64c412e049da
VictorCher <192.168.127.1@mail.ru>
2019-01-22 16:32:44 +0300

    Сделал два задания

--- AlgoritmicAndStuctureData/Program.cs
+++ AlgoritmicAndStuctureData/Program.cs
@@ -1,4 +1,4 @@
-﻿// Чернышов Виктор. Урок 3
+﻿// Чернышов Виктор. Урок 4
 
 using System;
 using System.Collections.Generic;
@@ -10,116 +10,63 @@ namespace AlgoritmicAndStuctureData
 {
     class Program
     {
-        static void Print(string msg, int[] array)
+        static void Task1()
         {
-            int[] mass = array;
-            Console.Write(msg);
-            for (int i = 0; i < mass.Length; i++)
+            /* Количество маршрутов с препятствиями. Реализовать чтение массива с препятствием и 
+             * нахождение количество маршрутов.*/
+            /*int[,] map = {
+                { 1, 1, 1},
+                { 0, 1, 0},
+                { 0, 1, 0}
+            };*/
+            int[,] map = {
+                { 1, 1, 1},
+                { 1, 1, 1},
+                { 1, 1, 1}
+            };
+            int x = 3, y = 3;
+            int[,] way = new int[x, y];
+            for (int j = 0; j < y; j++) if (map[0, j] == 1) way[0, j] = 1;
+            for (int i = 1; i < x; i++)
             {
-                Console.Write(mass[i] + " ");
-            }
-            Console.WriteLine();
-        }
-        static int Task1(int[] array)
-        {
-            /* Реализовать пузырьковую сортировку. Вывести количество операций, которые потребовались для сортировки.
-             */
-            int[] mass = new int[array.Length];
-            Array.Copy(array, mass, array.Length);
-            int count = 0;
-            Console.WriteLine("Пузырьковая сортировка");
-            Print("Массив до сортировки:          ", mass);
-            for (int i = 0; i < mass.Length; i++)
-            {
-                for (int j = 0; j < mass.Length - 1; j++)
+                if(map[i, 0] == 1) way[i, 0] = 1;
+                for (int j = 1; j < y; j++)
                 {
-                    if (mass[j] > mass[j + 1])
-                    {
-                        int x = mass[j];
-                        mass[j] = mass[j + 1];
-                        mass[j + 1] = x;
-                        count ++;
-                    }
-                    count++;
+                    if (map[i, j] == 1) way[i, j] = way[i, j - 1] + way[i - 1, j];
+                    else way[i, j] = 0; 
                 }
-                count++;
             }
-            Print("Массив после сортировки:       ", mass);
-            return count;
+            Console.WriteLine("Количество маршрутов равно: " + way[x - 1, y - 1]);
         }
-        static int Task2(int[] array)
+        static void Task2()
         {
-            /* Реализовать шейкерную сортировку. Вывести количество операций, которые потребовались для сортировки.
-             */
-            int[] mass = array;
-            int count = 0;
-            Console.WriteLine("Шейкерная сортировка");
-            Print("Массив до сортировки:          ", mass);
-            int left = 0;
-            int right = mass.Length - 1;
-            for (int i = 0; i < mass.Length/2; i++)
+            /* Решить задачу о нахождении длины максимальной подпоследовательности с помощью матрицы. */
+            string word1 = "GEEKMINDS";
+            string word2 = "GEEKBRAINS";
+            int len1 = word1.Length;
+            int len2 = word2.Length;
+            int[,] max = new int[len1+1, len2+1];
+
+            for (int i = 0; i < len1; i++)
             {
-                if (i % 2 == 0)
+                for (int j = 0; j < len2; j++)
                 {
-                    for (int j = left; j < right; j++)
+                    if (word1[i] == word2[j]) max[i+1, j+1] = max[i, j] + 1;
+                    else
                     {
-                        if (mass[j] > mass[j + 1])
-                        {
-                            int x = mass[j];
-                            mass[j] = mass[j + 1];
-                            mass[j + 1] = x;
-                            count++;
-                        }
-                        count++;
+                        if (max[i,j+1] > max[i+1,j]) max[i+1, j+1] = max[i, j+1];
+                        else max[i+1, j+1] = max[i+1, j];
                     }
-                    right--;
+                    Console.Write(max[i + 1, j + 1] + " ");
                 }
-                else
-                {
-                    for (int j = right; j > left; j--)
-                    {
-                        if (mass[j] < mass[j - 1])
-                        {
-                            int x = mass[j];
-                            mass[j] = mass[j - 1];
-                            mass[j - 1] = x;
-                            count++;
-                        }
-                        count++;
-                    }
-                    left++;
-                }
-                count+=2;
+                Console.WriteLine();
             }
-            Print("Массив после сортировки:       ", mass);
-            return count;
-        }
-        static int Task3(int value, int[] array)
-        {
-            /* Реализовать алгоритм поиска методом половинного деления в виде функции, которой передается 
-             * отсортированный массив.
-             * Функция должна возвращать индекс найденного элемента или -1, если элемент не найден.
-             */
-            int[] mass = array;
-            int left = 0;
-            int rigth = mass.Length - 1;
-            while (left <= rigth)
-            {
-                int i = left + (rigth - left) / 2;
-                if (mass[i] > value) rigth = i - 1;
-                else if (mass[i] < value) left = i + 1;
-                else return i;
-            }
-            return -1;
+            Console.WriteLine("Длина максимальной последовательности равна: " + max[len1, len2]);
         }
         static void Main(string[] args)
         {
-            int[] array = { 5, 3, 8, 1, 4, 10, 15, 2, 9, 7};
-            Console.WriteLine("Кол-во операций сравнения:     " + Task1(array));
-            Console.WriteLine("Кол-во операций сравнения:     " + Task2(array));
-            Console.Write("Какое число необходимо найти?: ");
-            int value = int.Parse(Console.ReadLine());
-            Console.WriteLine("Индекс искомого элемента:      " + Task3(value, array));
+            Task1();
+            Task2();
 
             Console.ReadLine();
         }
