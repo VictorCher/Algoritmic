acc6455c4380f7683f27449877001968c540f262
VictorCher <192.168.127.1@mail.ru>
2019-01-24 16:53:58 +0300

    Сделал задания 4, 5, 6

--- AlgoritmicAndStuctureData/Program.cs
+++ AlgoritmicAndStuctureData/Program.cs
@@ -8,6 +8,36 @@ using System.Threading.Tasks;
 
 namespace AlgoritmicAndStuctureData
 {
+    public class Node<T>
+    {
+        public T data;
+        public Node<T> Next;
+        public Node(T _data) { data = _data; }
+    }
+    public class ListStack<T>
+    {
+        Node<T> head;
+        int count;
+
+        public void Push(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            node.Next = head; 
+            head = node;
+            count++;
+        }
+        public T Pop()
+        {
+            Node<T> temp = head;
+            if (count == 0) Console.WriteLine("Стек пуст");
+            else
+            {
+                head = head.Next;
+                count--;
+            }
+            return temp.data;
+        }
+    }
     public class Stack<T>
     {
         const int stackSize = 10;
@@ -39,10 +69,32 @@ namespace AlgoritmicAndStuctureData
             return buf[0];
         }
     }
+    public class Queue<T>
+    {
+        const int masSize = 3;
+        T[] buf;
+        int front = -1;
+        int rear = -1;
+        public Queue()
+        {
+            buf = new T[masSize];
+        }
+        public void Enqueue(T value)
+        {
+            if (rear < masSize - 1) rear++;
+            else rear = 0;
+            buf[rear] = value;
+        }
+
+        public T Dequeue()
+        {
+            if (front < masSize - 1) front++;
+            else front = 0;
+            return buf[front];
+        }
+    }
     class Program
     {
-        
-
         static void Task1(int n)
         {
             /* Реализовать перевод из десятичной в двоичную систему счисления с использованием стека. */
@@ -84,11 +136,58 @@ namespace AlgoritmicAndStuctureData
             if (Stack.index == -1) Console.WriteLine("Введённая скобочная последовательность правильная");
             else Console.WriteLine("Введённая скобочная последовательность содержит ошибку");
         }
+        static void Task4(ListStack<string> stack)
+        {
+            /* Создать функцию, копирующую односвязный список (то есть создающую в памяти копию
+             * односвязного списка без удаления первого списка). */
+
+            ListStack<string> CopyStack = new ListStack<string>();
+            CopyStack = stack;
+        }
+        static void Task5(string line)
+        {
+            // Реализовать алгоритм перевода из инфиксной записи арифметического выражения в постфиксную.
+            Stack<char> Stack = new Stack<char>();
+            int a = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if ((line[i] == '+') || (line[i] == '-'))
+                {
+                    Stack.Push(line[i]);
+                    a++;
+                }
+                else Console.Write(line[i]);
+            }
+            for(int i = 0; i < a; i++) Console.Write(Stack.Pop());
+        }
+        static void Task6()
+        {
+            // Реализовать очередь с использованием массива.
+            Queue<string> Queue = new Queue<string>();
+            Queue.Enqueue("abcd");
+            Queue.Enqueue("efgh");         
+            string a = Queue.Dequeue();
+            string b = Queue.Dequeue();
+            Queue.Enqueue("ijkl");
+            Queue.Enqueue("mnop");
+            string c = Queue.Dequeue();
+            string d = Queue.Dequeue();
+            string e = Queue.Dequeue();
+        }
         static void Main(string[] args)
         {
             // Task1(536);
-            Task3("[2/{5*(4+7)}]");
-            
+            // Task3("[2/{5*(4+7)}]");
+            /*
+                        ListStack<string> stack = new ListStack<string>();
+                        stack.Push("Tom");
+                        stack.Push("Alice");
+                        stack.Push("Bob");
+                        stack.Push("Kate");
+                        Task4(stack);
+            */
+            // Task5("2+3-1");
+            Task6();
 
             Console.ReadLine();
         }
