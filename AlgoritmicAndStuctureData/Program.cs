2aebab6a6d8fb715190825cc069324615c7401fa
VictorCher <192.168.127.1@mail.ru>
2019-02-06 14:49:10 +0300

    Сделал 2 задания

--- AlgoritmicAndStuctureData/Program.cs
+++ AlgoritmicAndStuctureData/Program.cs
@@ -1,9 +1,8 @@
-﻿// Чернышов Виктор. Урок 7
+﻿// Чернышов Виктор. Урок 8
 
 /* 
- * 1. Написать функции, которые считывают матрицу смежности из файла и выводят её на экран.
- * 2. Написать рекурсивную функцию обхода графа в глубину.
- * 3. Написать функцию обхода графа в ширину
+ * 1. Реализовать сортировку подсчётом.
+ * 2. Реализовать быструю сортировку.
 */
 
 using System;
@@ -15,12 +14,71 @@ using System.Threading.Tasks;
 
 namespace AlgoritmicAndStuctureData
 {
-     class Program
+        class Program
     {
+        static int[] mas = { 1, 4, 7, 3, 6, 2, 9 };
+        static int[] CountingSort(int[] arr, int min, int max)
+        {
+            int[] count = new int[max - min + 1];
+            int z = 0;
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                count[i] = 0;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count[arr[i] - min]++;
+            }
+
+            for (int i = min; i <= max; i++)
+            {
+                while (count[i - min]-- > 0)
+                {
+                    arr[z] = i;
+                    z++;
+                }
+            }
+            return arr;
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        static void QSort(int[] array, int firstIndex = 0, int lastIndex = -1)
+        {
+            if (lastIndex < 0)
+                lastIndex = array.Length - 1;
+            if (firstIndex >= lastIndex)
+                return;
+            int middleIndex = (lastIndex - firstIndex) / 2 + firstIndex, currentIndex = firstIndex;
+            Swap(ref array[firstIndex], ref array[middleIndex]);
+            for (int i = firstIndex + 1; i <= lastIndex; ++i)
+            {
+                if (array[i] <= array[firstIndex])
+                {
+                    Swap(ref array[++currentIndex], ref array[i]);
+                }
+            }
+            Swap(ref array[firstIndex], ref array[currentIndex]);
+            QSort(array, firstIndex, currentIndex - 1);
+            QSort(array, currentIndex + 1, lastIndex);
+        }
+        static void Task1()
+        {
+            foreach (int i in CountingSort(mas, 0, 10)) Console.Write(i + " ");
+        }
+        static void Task2()
+        {
+            QSort(mas,0,mas.Length-1);
+            foreach (int i in mas) Console.Write(i + " ");
+        }
         static void Main(string[] args)
         {
-            Graf.CreateGraf();
-            Graf.InWidth(Graf.ReadGraf(), 0);
+            Task1();
+            Task2();
 
             Console.ReadLine();
         }
